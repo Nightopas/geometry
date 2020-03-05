885e462a8899a1200625ef881bba4ec225b51d56
Каменев Павел Игоревич <ip913s12@awp02.csc.local>
2020-03-05 13:56:28 +0700

    Updated how the code works

---
+++ circle.c
@@ -0,0 +1,11 @@
+#include "math.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+double circle(double x, double y, double r)
+{
+    printf("\n perimetr - %f", 2 * M_PI * r);
+    printf("\n area - %f", M_PI * r * r);
+    printf("\n    intersects \n");
+    return (float)2 * M_PI * r;
+}
