d896a8112cd27e9ae7b75c945940a1124fc88968
Каменев Павел Игоревич <ip913s12@awp02.csc.local>
2020-03-05 15:07:47 +0700

    Added a function input

--- geomcode.c
+++ geomcode.c
@@ -5,11 +5,22 @@
 
 int main()
 {
-    int x, y;
-    float r;
-    scanf("%d %d %f", &x, &y, &r);
-    printf("circle(%d %d, %f", x, y, r);
-    printf(")");
-    circle(x, y, r);
+    while (1) {
+        char s[10];
+        scanf("%s", s);
+
+        if (strcmp(s, "circle") == 0) {
+            printf("\n   %s", s);
+            int x, y;
+            float r;
+            scanf("%d %d %f", &x, &y, &r);
+            printf("circle(%d %d, %f", x, y, r);
+            printf(")");
+            circle(x, y, r);
+        }
+        if (strcmp(s, "0") == 0) {
+            return 1;
+        }
+    }
 }
 
